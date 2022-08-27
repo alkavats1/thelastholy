d6ce14edfc49009efeb5b766327a0d0cac7c3b34
alkavats1 <alka.vats@knoldus.com>
2022-08-27 12:50:38 +0530

    docs folder added

---
+++ docs/856.1ddc71b7ec9a19ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklastholy=self.webpackChunklastholy||[]).push([[856],{2856:(c,a,l)=>{l.r(a),l.d(a,{DecoratedAmbulanceModule:()=>_});var e=l(6895),d=l(6081),t=l(445),n=l(2472),u=l(8256);let _=(()=>{class o{}return o.\u0275fac=function(E){return new(E||o)},o.\u0275mod=u.oAB({type:o}),o.\u0275inj=u.cJS({imports:[e.ez,d.S,n.AboutModule,t.GalleryModule]}),o})()}}]);

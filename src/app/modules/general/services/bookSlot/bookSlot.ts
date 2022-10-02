71e7f1e851ec1c65249d765ef35dd14fdc3c5791
alkavats1 <alka.vats@knoldus.com>
2022-10-02 21:58:11 +0530

    Book slot form refactored

--- src/app/modules/general/services/bookSlot/bookSlot.ts
+++ src/app/modules/general/services/bookSlot/bookSlot.ts
@@ -16,3 +16,17 @@ export class BookSlotRequest {
     this.noOfPeople = noOfPeople
   }
 }
+
+export class DateOfBook {
+  day: number;
+  month: number;
+  year: number;
+
+  constructor(day: number,
+              month: number,
+              year: number) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+}

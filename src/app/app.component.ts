539b9206386d512ad93051a487091550af228b7c
alkavats1 <alka.vats@knoldus.com>
2022-10-03 12:08:08 +0530

    PL-01 | GTA added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppConstants} from "./app.constant";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'The Last Holy Journey';
+
+
+  ngOnInit(): void {
+    this.setPanelGtag();
+  }
+
+  setPanelGtag() {
+      const gtagPanelScript = document.createElement('script');
+      gtagPanelScript.async = true;
+      gtagPanelScript.src = AppConstants.gtagPanelScriptSrc;
+      document.getElementsByTagName('head')[0].appendChild(gtagPanelScript);
+      const gtagDataLayer = document.createElement('script');
+      gtagDataLayer.innerHTML = AppConstants.gtagPanelDataLayer;
+      document.getElementsByTagName('head')[0].appendChild(gtagDataLayer);
+
+  }
 }
